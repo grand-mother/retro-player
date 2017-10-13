156082f1e947f74b30058be3c8f68f68cf20eff5
Valentin Niess <niess@in2p3.fr>
2017-10-13 22:58:33 +0200

    Add an event manager with iteration over decays.

--- scripts/play.py
+++ scripts/play.py
@@ -19,6 +19,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+import collections
 import json
 import sys
 import time
@@ -30,6 +31,112 @@ import numpy
 import puppy.build
 import puppy.control
 import puppy.texture
+
+class EventIterator:
+    """Iterator over GRAND events stored in a JSON file."""
+    def __init__(self, path):
+        f = open(path)
+        self._file = f
+        self._prev = -1
+        self._current = None
+
+    def __del__(self):
+        if self._file:
+            self._file.close()
+            self._file = None
+
+    def __iter__(self):
+        self.rewind()
+        return self
+
+    def next(self):
+        """Pop the next event from file."""
+        try: event = json.loads(self._file.readline())
+        except ValueError: raise StopIteration()
+        self._prev = event["previous"]
+        self._current = event
+        return event
+
+    def previous(self):
+        """Pop the previous event from file."""
+        if self._prev >= 0: self._file.seek(self._prev, 0)
+        else: self._file.seek(0, 0)
+        return self.next()
+
+    def rewind(self):
+        """Rewind the file."""
+        self._file.seek(0, 0)
+        self._prev = -1
+        self._current = None
+
+class EventManager(object):
+    """Manage GRAND events."""
+    def __init__(self, path=None, on_start=None):
+
+        if path: self.events = EventIterator(path)
+        self._current_vertex = None
+        self._all_vertices = collections.deque([])
+        if on_start:
+            for process in on_start:
+                try: method, kwargs = process
+                except ValueError: method, kwargs = process, {}
+                getattr(self, method)(**kwargs)
+
+        # Register the extra event handlers.
+        self.accept("raw-e", self.show_next_event)
+        self.accept("raw-q", self.show_previous_event)
+        self.accept("raw-t", self.toggle_all_decays)
+
+    def toggle_all_decays(self):
+        """Toggle the display of all the tau decay vertices."""
+        t0 = time.time()
+        print "o Toggling all tau decays ..."
+        if self._all_vertices:
+            while True:
+                try: node = self._all_vertices.pop()
+                except IndexError: break
+                node.removeNode()
+        else:
+            for event in self.events:
+                self._all_vertices.append(self._render_decay(event))
+            self.events.rewind()
+        print "  -> Done in {:.1f}s".format(time.time() - t0)
+
+    def show_next_event(self):
+        """Render the next event and relocate to it."""
+        try: event = self.events.next()
+        except StopIteration: event = self.events.current
+        self._update_event(event)
+
+    def show_previous_event(self):
+        """Render the previous event and relocate to it."""
+        event = self.events.previous()
+        self._update_event(event)
+
+    def _render_decay(self, event):
+        """Render a decay."""
+        energy, position, direction, _, _  = event["tau_at_decay"]
+        size = numpy.log10(energy)
+        vertex = puppy.build.Box(
+          size, face_color=(1,1,0,1), line_color=(0,0,0,1)).render()
+        vertex.setPos(*position)
+        edges = (position, position - 100. * numpy.array(direction))
+        track = puppy.build.Track(edges, line_color=(1,1,0,1)).render()
+        vertex.reparentTo(track)
+        return track
+
+    def _update_event(self, event):
+        """Update the current event and relocate to it."""
+        self._relocate_camera(event)
+        if not self._all_vertices:
+            if self._current_vertex: self._current_vertex.removeNode()
+            self._current_vertex = self._render_decay(event)
+
+    def _relocate_camera(self, event):
+        """Relocate the camera to the given event."""
+        position = event["tau_at_decay"][1] + numpy.array((100., 100., 100.))
+        self.camera.setPos(*position)
+        self.camera.lookAt(*event["tau_at_decay"][1])
 
 def Paint():
     """Closure for texturing.
@@ -43,7 +150,7 @@ def Paint():
         try:
             return textures[name]
         except KeyError:
-            path = "shared/textures/{:}.jpg".format(name)
+            path = "share/textures/{:}.jpg".format(name)
             t = puppy.texture.load(path)
             textures[name] = t
             return t
@@ -67,13 +174,14 @@ def Paint():
     return paint
 paint = Paint()
 
-class Player(puppy.control.KeyboardCamera):
+class Player(puppy.control.KeyboardCamera, EventManager):
     """Interactive event display for GRAND.
     """
-    def __init__(self, topography=None, comment=None):
+    def __init__(self, topography=None, events=None, comment=None):
         """Initialise the player.
         """
         puppy.control.KeyboardCamera.__init__(self)
+        EventManager.__init__(self, **events)
 
         # Initialise the topography provider.
         if topography:
@@ -115,7 +223,7 @@ class Player(puppy.control.KeyboardCamera):
             self.camera.setPos(0., 0., topo.ground_altitude(0., 0.) + 1.7)
 
         # Add a background image.
-        OnscreenImage(parent=render2d, image="shared/textures/stars.jpg")
+        OnscreenImage(parent=render2d, image="share/textures/stars.jpg")
         base.cam.node().getDisplayRegion(0).setSort(20)
 
 if __name__ == "__main__":
